bad216bc73dd6f07884e534d3c1f9738b351859f
salah alhashmi <alhashmis246@gmail.com>
2022-02-01 12:35:43 +0400

    update

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 import { Volder, Email } from 'volder';
 
+const form = document.querySelector('form');
+const usernameErr = document.querySelector('p.username.error');
+const emailErr = document.querySelector('p.email.error');
+const paswordErr = document.querySelector('p.password.error');
 
 // initializing volder schema for login form.
-
-
 const loginSchema = new Volder({
     username: {
-        type: [String, 'username must be in string'],
-        alphanumeric: [true, 'username should only contain letters and numbers'],
-        minLength: [4, 'username at least 4 characters'],
-        maxLength: [16, 'username at most 16 characters'],
-        required: [true, 'username is required'],
+        type: [String, '⚠️ username must be in string'],
+        alphanumeric: [true, '⚠️ username should only contain letters and numbers'],
+        minLength: [4, '⚠️ username should be at leat 4 characters'],
+        maxLength: [16, '⚠️ username should be at most 16 characters'],
+        required: [true, '⚠️ username is required'],
         trim: true
     },
     email: {
-        type: [String ,'email must be in string'],
-        pattern: [Email, 'not valid email'],
-        maxLength: [150, 'email be at most 150 characters'],
-        required: [ true,'email is required'],
+        type: [String ,'⚠️ email must be in string'],
+        pattern: [Email, '⚠️ email is not valid'],
+        maxLength: [150, '⚠️ email be at most 150 characters'],
+        required: [ true,'⚠️ email is required'],
         trim: true
     },
     password: {
-        type: [String , 'password must be in string'],
-        minLength:[8, 'password should be at least 8 characters'],
-        maxLength: [30, 'password should be at most 30 characters'],
-        required: [ true, 'password is required'],
-        matches: ["^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]$", 'password must contain numbers and letters']
+        type: [String , '⚠️ password must be in string'],
+        minLength:[8, '⚠️ password should be at least 8 characters'],
+        maxLength: [30, '⚠️ password should be at most 30 characters'],
+        required: [ true, '⚠️ password is required'],
+        matches: ["^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]$", '⚠️ password must contain numbers and letters and symbols']
     }
+})
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  console.log(form.get('username'))
 })
